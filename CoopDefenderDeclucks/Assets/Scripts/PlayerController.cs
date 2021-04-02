5a71df5b375e05881746bd325fa2f90f730757fb
jafrezzell <jafrezzell@msn.com>
2021-04-02 16:35:58 -0400

    Power-up timers implemented

--- CoopDefenderDeclucks/Assets/Scripts/PlayerController.cs
+++ CoopDefenderDeclucks/Assets/Scripts/PlayerController.cs
@@ -12,11 +12,27 @@ public class PlayerController : MonoBehaviour
     private Vector3 moveVelocity;
 
     private Vector3 lookPoint;
-
+    
     private Camera mainCamera;
+
+    public float timeSlowAmount;//Duration of Time Slow Powerup
+    private float timeSlowTimer;
+    public bool isTimeSlowed;
+    
+    public float spreggShotAmount;//Duration of Spregg Shot Powerup
+    private float spreggShotTimer;
+    public bool isSpregg;
+
+    public float rapidFireAmount;//Duration of Rapid Fire Powerup
+    private float rapidFireTimer;
+    public bool isRapidFire;
+
     // Start is called before the first frame update
     void Start()
     {
+        timeSlowTimer = 0;
+        spreggShotTimer = 0;
+        rapidFireTimer = 0;
         myRigidbody = GetComponent<Rigidbody>();
         mainCamera = FindObjectOfType<Camera>();
         lookPoint = transform.forward;
@@ -27,8 +43,40 @@ public class PlayerController : MonoBehaviour
     {
         moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput * moveSpeed;
-     
-       
+
+        #region Code for Power-Up Timers
+        if (timeSlowTimer > 0)//Checks to see if time slow power up is active
+        {
+            timeSlowTimer -= Time.deltaTime/Time.timeScale;
+            if (timeSlowTimer <= 0)//Return time scale to normal after elapsed power-up time
+            {
+                timeSlowTimer = 0;
+                isTimeSlowed = false;
+                Time.timeScale = 1;
+            }
+        }
+        if(spreggShotTimer > 0)
+        {
+            spreggShotTimer -= Time.deltaTime / Time.timeScale;
+            if(spreggShotTimer <= 0)
+            {
+                spreggShotTimer = 0;
+                isSpregg = false;
+            }
+
+        }
+        if(rapidFireTimer > 0)
+        {
+            rapidFireTimer -= Time.deltaTime / Time.timeScale;
+            if(rapidFireTimer <= 0)
+            {
+                rapidFireTimer = 0;
+                isRapidFire = false;
+            }
+        }
+        #endregion
+
+
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up,Vector3.zero);
         float rayLength;
@@ -47,9 +95,36 @@ public class PlayerController : MonoBehaviour
         //myRigidbody.AddForce(moveVelocity);
         moveVelocity.y = myRigidbody.velocity.y;
         myRigidbody.velocity = moveVelocity;
+        
+
         if (Input.GetMouseButton(0))
         {
             gun.shoot(lookPoint);
         }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        string otherTag = other.tag;//Tag of object that player collided with
+        switch (otherTag)//Switch statement checks if player collided with a power-up based on tag
+        {
+            case "TimeSlow":
+                isTimeSlowed = true;
+                timeSlowTimer += timeSlowAmount;
+                Time.timeScale = 0.5f;
+                Destroy(other.gameObject);
+                break;
+            case "SpreggShot":
+                isSpregg = true;
+                spreggShotTimer += spreggShotAmount;
+                Destroy(other.gameObject);
+                break;
+            case "RapidFire":
+                isRapidFire = true;
+                rapidFireTimer += rapidFireAmount;
+                Destroy(other.gameObject);
+                break;
+        }
+    }
+
 }
