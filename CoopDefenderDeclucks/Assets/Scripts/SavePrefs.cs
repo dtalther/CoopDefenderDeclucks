d2b9188fc5824eeb5eeb0f1518d9e544790dbe50
jafrezzell <jafrezzell@msn.com>
2021-04-16 18:20:24 -0400

    Save Functionality and Main menu scores
    Created base for saving scores and main menu value

---
+++ CoopDefenderDeclucks/Assets/Scripts/SavePrefs.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePrefs : MonoBehaviour
+{
+    int highScore;
+
+    public void SaveScore(int newScore)
+    {
+        highScore = newScore;
+        PlayerPrefs.SetInt("HighScoreData", highScore);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadGameData()
+    {
+        if (PlayerPrefs.HasKey("HighScoreData"))
+        {
+            highScore = PlayerPrefs.GetInt("HighScoreData");
+            Debug.Log("Game data loaded!");
+        }
+        else
+        {
+            Debug.Log("THERE IS NO SAVE DATA!");
+            highScore = 0;
+        }
+    }
+
+    //Gets the best score the player has gotten
+    public int GetBestScore()
+    {
+        return highScore;
+    }
+}
