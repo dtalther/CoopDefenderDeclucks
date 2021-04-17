c73a29fb766d12b800240d2b71652e9f83c66412
Kevimo <krmoran77@gmail.com>
2021-04-17 16:03:14 -0400

    CampaignLevel0 can be beaten

---
+++ CoopDefenderDeclucks/Assets/Scripts/NextLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider change)
+    {
+        if (change.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
