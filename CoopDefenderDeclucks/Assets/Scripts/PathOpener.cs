c1290ada63c1941bcb03eb1096f6e172bce7d19f
Kevimo <krmoran77@gmail.com>
2021-04-18 01:12:15 -0400

    Updated Level 0 and Level 1

---
+++ CoopDefenderDeclucks/Assets/Scripts/PathOpener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathOpener : MonoBehaviour
+{
+    public float timeUntilDestruction;
+    // Update is called once per frame
+    void Update()
+    {
+        timeUntilDestruction -= Time.deltaTime;
+        if (timeUntilDestruction <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
