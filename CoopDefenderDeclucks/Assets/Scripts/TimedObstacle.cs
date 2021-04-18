735bae8a8cb40a5d8b7082a9d85ed89ec0378691
Kevimo <krmoran77@gmail.com>
2021-04-18 15:01:29 -0400

    Update to Level 1, added timed obstacles

---
+++ CoopDefenderDeclucks/Assets/Scripts/TimedObstacle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedObstacle : MonoBehaviour
+{
+    public float timer;
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
