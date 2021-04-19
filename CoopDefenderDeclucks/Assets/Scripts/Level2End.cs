eb9f3ad8f34c4e3225e1e7ae5cf5c3db6c0aa762
Daniel Alther <35381675+dtalther@users.noreply.github.com>
2021-04-19 01:15:23 -0400

    Fixed Bugs

---
+++ CoopDefenderDeclucks/Assets/Scripts/Level2End.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level2End : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool ruthRescued;
+    public MainMenu menuManager;
+    public GameObject arrow;
+    void Start()
+    {
+        ruthRescued = false;
+        menuManager = GameObject.FindObjectOfType<MainMenu>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ally")
+        {
+            ruthRescued = true;
+            arrow.SetActive(true);
+        }            
+        if(other.gameObject.tag == "Player" && ruthRescued)
+        {
+            print("Level Complete");
+            menuManager.startMode(6);
+        }
+    }
+}
